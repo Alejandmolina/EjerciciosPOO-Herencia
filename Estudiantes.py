a1a9b226061974bf82f68baf9ba110502d55de7e
Alejandro Molina <playrenealejand@gmail.com>
2023-03-10 09:32:55 -0600

    Ejercicio1 - Registro de notas

---
+++ Estudiantes.py
@@ -0,0 +1,6 @@
+class Estudiantes:
+    def __init__(self, nombre, edad, genero, matricula):
+        self.nombre = nombre
+        self.edad = edad
+        self.genero = genero
+        self.matricula = matricula
